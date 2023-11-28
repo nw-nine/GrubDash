879d93867fa45b154fdc2a76a923f74c88a22d14
Nate Watkins <natewatkinsca@gmail.com>
2023-11-28 09:58:43 -0800

    function list and create working for orders controller

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,62 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function list (req, res, next) {
+    res.send({ data: orders })
+}
+function validateOrder(req, res, next) {
+    const { deliverTo, mobileNumber, dishes } = req.body.data;
+    if (!deliverTo) {
+        return res.status(400).json({ error: 'Order must include a deliverTo' });
+    }
+    if (!mobileNumber) {
+        return res.status(400).json({ error: 'Order must include a mobileNumber' });
+    }
+    if (!dishes || !Array.isArray(dishes) || dishes.length === 0) {
+        return res.status(400).json({ error: 'Order must include at least one dish' });
+    }
+    dishes.forEach((dish, index) => {
+        const { quantity } = dish;
+        if (!quantity || quantity <= 0 || !Number.isInteger(quantity)) {
+            return res.status(400).json({ error: `Dish ${index} must have a quantity that is an integer greater than 0` });
+        }
+    });
+    next();
+}
+
+function create(req, res, next) {
+    const { deliverTo, mobileNumber, status, dishes } = req.body.data;
+    const newOrder = {
+        id: nextId(),
+        deliverTo,
+        mobileNumber,
+        status,
+        dishes,
+    }
+    orders.push(newOrder)
+
+    res.status(201).json({ data: newOrder })
+}
+
+function validateOrderExists(req, res, next) {
+    let { id } = req.params;
+    let index = orders.findIndex(order => order.id === id);
+    if (index < 0) {
+      // bad news
+      next({
+        status: 404,
+        message: `could not find order with id ${id}`
+      })
+    } else {
+      // we found it! save its location for later
+      res.locals.index = index;
+      next();
+    }
+  }
+
+module.exports = {
+    list,
+    create: [validateOrder, create],
+
+
+}
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
+const controller = require('./orders.controller')
 
 // TODO: Implement the /orders routes needed to make the tests pass
+
+router.route('/')
+    .get(controller.list)
+    .post(controller.create)
+
+router.route('/:orderId')
 
 module.exports = router;
