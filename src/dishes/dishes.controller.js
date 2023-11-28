933abed09196b8738d40862408c6dfca0340527f
Nate Watkins <natewatkinsca@gmail.com>
2023-11-28 07:58:41 -0800

    made list function and create function for dishes controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,38 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res, next) {
+    res.send({ data: dishes })
+}
+
+function create(req, res, next) {
+    const { name, description, price, image_url } = req.body.data;
+    if (!name) {
+        return res.status(400).json({ error: 'Dish must include a name' });
+    }
+    if (!description) {
+        return res.status(400).json({ error: 'Dish must include a description' });
+    }
+    if (!price || price <= 0 || !Number.isInteger(price)) {
+        return res.status(400).json({ error: 'Dish must have a price that is an integer greater than 0' });
+    }
+    if (!image_url) {
+        return res.status(400).json({ error: 'Dish must include an image_url' });
+    }
+    const newDish = {
+        id: nextId,
+        name,
+        description,
+        price,
+        image_url
+    }
+    dishes.push(newDish)
+
+    res.status(201).json({ data: newDish })
+}
+
+module.exports = {
+    list,
+    create,
+    
+}
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
+const controller = require('./dishes.controller')
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+
+router.route('/')
+    .get(controller.list)
+    .post(controller.create)
 
 module.exports = router;
